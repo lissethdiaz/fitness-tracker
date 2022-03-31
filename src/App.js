501713f5e981fe12006f29c47cb7096543e9e93a
Aaron Bean <arbean1978@gmail.com>
2022-03-31 09:04:13 -0600

    updateAppjsindexjsloginjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Switch,Route, Redirect} from 'react-router-dom';
 import './App.css';
+import NavTabs from './components/NavTabs';
+import Goals from './pages/Goals';
+import History from './pages/History';
+import Login from './pages/Login';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  function App() {
+    const [isLoggedIn,setIsLoggedIn] = useState(false)
+  
+    const toggleLogin = () => {
+      setIsLoggedIn(prevState => prevState === true ? false : true)
+    }
+
+    return (
+      <div className="App">
+        <NavTabs isLoggedIn={isLoggedIn} />
+        <div className='container'>
+          {/* App routing switch goes here :) */}
+          <Switch>
+            <Route exact path='/'>
+              <Login isLoggedIn={isLoggedIn} toggleLogin={toggleLogin} />
+            </Route>
+            <Route exact path='/home'>
+              {isLoggedIn ? <Home /> : <Redirect to='/' />}
+            </Route>
+            <Route exact path='/history'>
+              {isLoggedIn ? <History /> : <Redirect to='/' />}
+            </Route>
+            <Route exact path='/goals'>
+              {isLoggedIn?<Goals /> : <Redirect to='/'/>}
+            </Route> 
+          </Switch>
+        </div>
+        <Footer />
+      </div>
   );
 }
 
-export default App;
+export default App
