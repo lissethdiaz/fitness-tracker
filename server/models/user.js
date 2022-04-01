d676ec210125c5063044829d28f0c2403c641198
Wenwen Tian <wwtian9@gmail.com>
2022-03-31 20:09:55 -0600

    add bcypt to protect user info

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,11 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
   username: {
     type: String,
     trim: true,
+    unique: true,
     required: "Username is Required",
   },
   password: {
@@ -12,18 +14,31 @@ const UserSchema = new Schema({
     required: "Password is Required",
     minlength: 6,
   },
-
   email: {
     type: String,
     unique: true,
     match: [/.+@.+\..+/],
   },
-
   userCreated: {
     type: Date,
     default: Date.now,
   },
 });
+
+// hash user password
+UserSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+UserSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model("User", UserSchema);
 
