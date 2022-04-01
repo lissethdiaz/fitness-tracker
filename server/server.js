7e2317bcabf5ffab178b698f77c70484837029cc
Wenwen Tian <wwtian9@gmail.com>
2022-03-31 19:46:43 -0600

    add mongoose connection

--- server/server.js
+++ server/server.js
@@ -1,31 +1,28 @@
-const express = require('express');
-const path = require('path');
-const routes = require("./routes")
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
 
-const db = require('./config/connection');
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
-	router.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client/build/index.html'));
-	});
-
+  router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 }
 
-app.use(routes)
+app.use(routes);
 
-db.once('open', () => {
-	app.listen(PORT, () => {
-		console.log(`API server running on port ${PORT}!`);
-	});
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
