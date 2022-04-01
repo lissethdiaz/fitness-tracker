40e4d763bce3b30dfac763c42fd95e6ef80f621f
Aaron Bean <arbean1978@gmail.com>
2022-04-01 09:59:57 -0600

    more

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import NavTabs from './components/NavTabs'
 import Goals from './pages/Goals'
 import History from './pages/History'
-import LoginPage from './pages/Login'
+import Login from './pages/Login'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Footer from './components/Footer'
@@ -24,23 +24,11 @@ function App() {
 
     return (
       <div className="App">
-        <NavTabs isLoggedIn={isLoggedIn} />
-        <LoginPage isLoggedIn={isLoggedIn} toggleLogin={toggleLogin} />
-        <div className='container bg-black'>
-          {/* App routing goes here :) */}
           <Routes>
-            <Route path='/'> {isLoggedIn ? <LoginPage/> : <Navigate to='/' />}
-                <Route path=':signup'> {isLoggedIn ? <Signup /> : <Navigate to=':signup' />}
-                </Route>
-            </Route>
-            <Route path='/home'>{isLoggedIn ? <Home /> : <Navigate to='/' />}
-            </Route>
-            <Route  path='/history'>{isLoggedIn ? <History /> : <Navigate to='/' />}
-            </Route>
-            <Route  path='/goals'>{isLoggedIn?<Goals /> : <Navigate to='/'/>}
-            </Route> 
-          </Routes>
-        </div>
+            <Route exact path='/' component={Login} /> 
+        
+            </Routes>
+  
         <Footer></Footer>
       </div>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,45 +1,128 @@
 import React from 'react'
-import ReactDOM from 'react-domgi'
-import {useNavigate, Outlet} from 'react-router-dom'
+import ReactDOM from 'react-dom'
+//import {useNavigate, Outlet} from 'react-router-dom'
 import NavTabs from '../components/NavTabs'
-import Signup from './Signup'
+//import Signup from './Signup'
 
+class Login  extends React.Component {
 
-function LoginPage({isLoggedIn,toggleLogin}) {
-  const navigate = useNavigate()
+  constructor(props) {
+    super(props);
+    this.state = { isLoginOpen: true, isRegisterOpen: false};
+  }
+
+showLoginBox() {
+  this.setState({isLoginOpen: true, isRegisterOpen: false});
+}
   
-  function handleClick() {
-    toggleLogin()
-    if (!isLoggedIn) {
-      navigate.push('/signup');
-    }
+showRegisterBox() {
+  this.setState({isRegisterOpen: true, isLoginOpen: false });
+}
+
+render() {
+
+  return (
+
+    <div className='root-container'>
+      <div className='box-controller'>
+        <div className={'controller' + (this.state.isLoginOpen ? 'selected-controller' : '')}  onClick={this.showLoginBox.bind(this)}>
+          Login
+        </div>
+        <div className={'controller' + (this.state.isRegisterOpen ? 'selected-controller' : '')}  onClick={this.showRegisterBox.bind(this)}>
+          Register
+        </div>
+      </div>
+      <div className='box-container'>
+        {this.state.isLoginOpen && LoginBox}
+        {this.state.isRegisterOpen && RegisterBox}
+      </div>
+    </div> 
+  );
+}
+}
+
+class LoginBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  submitLogin(e) {
+
   }
 
 
-
+render() {
   return (
-    <div className='container'>
-      <NavTabs 
-        pageTitle={isLoggedIn ? "Log Out" : "Login"} 
-        pageIntroText={"Welcome to your Fitness Tracker. Please log in to continue."}
-      />
-      <main className='container'>
-        {isLoggedIn ?  
-            <button type="button" className="btn" onClick={handleClick}>
-              Log Out
-            </button>
-            : 
-            <button type="button" className="btn" onClick={handleClick}>
-              Log In
-            </button>
-            }
-            <button type='button' className='btn' onClick={Signup}>
-                No account? Create one!
-            </button>
-        <Outlet></Outlet>
-      </main>
+  <div className='inner-container'>
+    <div className='header'>
+      Login
     </div>
-  )
+    <div className='box'>
+
+      <div className='input-group'>
+        <label htmlFor='username'>Username</label>
+        <input type='text' name='username' className='login-input' placeholder='Username'/>
+      </div>
+
+      <div className='input-group'>
+        <label htmlFor='password' >Password</label>
+        <input type='password' name='password' className='login-input' placeholder='Password'/>
+      </div>
+
+      <button type='button' className='login-btn' onClick= {this.submitLogin.bind(this)}>Login</button>
+
+    </div>
+
+  </div>
+  );
+}
 }
 
-export default LoginPage
+class RegisterBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  submitRegister(e) {
+
+  }
+
+
+render() {
+  return (
+  <div className='inner-container'>
+    <div className='header'>
+      Register
+    </div>
+
+    <div className='box'>
+
+      <div className='input-group'>
+        <label htmlFor='username'>Username</label>
+        <input type='text' name='username' className='login-input' placeholder='Username'/>
+      </div>
+
+      <div className='input-group'>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='emaile' className='login-input' placeholder='Email'/>
+      </div>
+
+      <div className='input-group'>
+        <label htmlFor='password' >Password</label>
+        <input type='password' name='password' className='login-input' placeholder='Password'/>
+      </div>
+
+      <button type='button' className='login-btn' onClick= {this.submitRegister.bind(this)}>Sign Up!</button>
+
+    </div>
+
+  </div>
+  );
+}
+}
+
+export default Login
