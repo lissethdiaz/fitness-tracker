73ca7306f95cabbf21a29fd49d60bbd1a2fc45f2
Wenwen Tian <wwtian9@gmail.com>
2022-03-31 20:40:51 -0600

    fixed bugs on user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,5 @@
 const router = require("express").Router();
-const {
-  createUser,
-  getSingleUser,
-  login,
-} = require("../controllers/user-controller");
+const { createUser, getSingleUser, login } = require("../controllers/user");
 
 // import middleware
 const { authMiddleware } = require("../utils/auth");
