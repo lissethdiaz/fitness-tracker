cced47394beb1bb9e1b52db5ee03b94efa1a7c5a
Liss Diaz <lissdiaz15@gmail.com>
2022-04-03 14:58:57 -0600

    edited login

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,14 +16,6 @@ export default class Login extends Component {
               <label>Password</label>
               <input type="password" className="form-control" placeholder="Enter password" />
           </div>
-
-          <div className="form-group">
-              <div className="custom-control custom-checkbox">
-                  <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                  <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-              </div>
-          </div>
-
           <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
       </form>
         );
