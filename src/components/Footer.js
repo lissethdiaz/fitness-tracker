7f6c35a17d81a9f9a5222a3bc6a5f08f6e9e1cbd
Aaron Bean <arbean1978@gmail.com>
2022-04-05 08:10:01 -0600

    goalstentative

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='container flex justify-center items-center text-5xl font-bold tracking-wide text-center shadow-lg h-16 font-josefin-sans'>
+    <footer className='container flex justify-center items-center text-xs font-bold tracking-wide text-center shadow-lg h-16 font-josefin-sans'>
       Fit don't Quit by{' '}
     </footer>
   )
