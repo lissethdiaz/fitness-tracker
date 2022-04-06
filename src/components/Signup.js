c13fd6bb0fc2cd133c8280109c018b45bc3e197a
kaisankyachan <kaisankyachan@gmail.com>
2022-04-05 20:37:26 -0600

    added component code

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import {authenticationService } from '../services/authentication.service';
+
 import axios from 'axios';
 import Login from './Login';
 import { Link } from 'react-router-dom';
-import {render} from 'react-dom'
 
-export  class Signup extends React.Component {
+export default class Signup extends Component {
     constructor(props) {
         super(props);
 
-        if(authenticationService.currentUserValue) {
-            this.props.history.push('/');
-        }
-    
         this.onChangeUsername = this.onChangeUsername.bind(this);
         this.onChangeEmail = this.onChangeEmail.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
@@ -33,7 +28,7 @@ export  class Signup extends React.Component {
 
     onChangeEmail(e) {
         this.setState({
-           email: e.target.value
+            email: e.target.value
         })
     }
 
@@ -41,15 +36,16 @@ export  class Signup extends React.Component {
         this.setState({
             password: e.target.value
         })
-   }
+    }
 
-    //Login() {
-    //}
+    Login() {
+
+    }
 
     onSubmit(e) {
         e.preventDefault();
 
-       const user = {
+        const user = {
             username: this.state.username
         }
 
@@ -66,15 +62,15 @@ export  class Signup extends React.Component {
         axios.post('http://localhost:3000/user/add', user)
             .then(res => console.log(res.data));
 
-       this.setState({
+        this.setState({
             username: '',
             email: '',
             password: ''
         })
     }
+
     render() {
         return (
-
             <div className='container h-auto bg-blue-200'>
                 <h3 className='flex justify-center items-center text-lg font-bold tracking-wide text-center shadow-lg h-16 font-josefin-sans bg-fuchsia-100'>Let's Get Started!</h3>
                 <form className='align-middle flex flex-col  justify-center items-center' onSubmit={this.onSubmit}>
@@ -89,24 +85,38 @@ export  class Signup extends React.Component {
                         />
                     </div>
 
-          <div className="form-group">
-              <label>First name</label>
-              <input type="text" className="form-control" placeholder="First name" />
-          </div>
+                    <div className="form-group">
+                        <label className='font-bold'>Email Address: </label>
+                        <input type="email"
+                            required
+                            className="form-control rounded border-double flex justify-center items-center"
+                            value={this.state.email}
+                            placeholder="enter your email address"
+                            onChange={this.onChangeEmail}
+                        />
+                    </div>
 
-          <div className="form-group">
-              <label>Last name</label>
-              <input type="text" className="form-control" placeholder="Last name" />
-          </div>
+                    <div className="form-group">
+                        <label className='font-bold'>Password: </label>
+                        <input type="text"
+                            required
+                            className="form-control rounded border-double flex justify-center items-center"
+                            value={this.state.password}
+                            placeholder="enter a unique password"
+                            onChange={this.onChangePassword}
+                        />
+                    </div>
+                    <div className='h-20 justify-center items-center pt-5 pb-5'>
+                        <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Let's Go!</button>
+                    </div>
+                    <div className=' h-20 justify-center items-center pt-5 pb-5 font-bold'>
+                        Or...
+                    </div>
 
-          <div className="form-group">
-              <label>Email</label>
-              <input type="email" className="form-control" placeholder="Enter email" />
-          </div>
+                    <Link to='/login' ><button type='button' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded' >Login</button></Link>
 
                 </form>
             </div>
-    
         )
     }
 }
